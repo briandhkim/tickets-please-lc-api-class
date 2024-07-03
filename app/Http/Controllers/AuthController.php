13625a9f6c55117845a265d07d3df147b9d8bb33
Brian Kim <briandhkim93@gmail.com>
2024-07-02 22:21:35 -0700

    Ep2 - additional intro routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,13 @@ class AuthController extends Controller
 {
     use ApiResponses;
 
-    public function login() {
-        return $this->ok('login test');
+    public function login(ApiLoginRequest $request) 
+    {
+        return $this->ok($request->get('email'));
+    }
+
+    public function register()
+    {
+        return $this->ok('register test abc');
     }
 }
