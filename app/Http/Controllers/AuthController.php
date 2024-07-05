ef572f6c6c88b760f7845c0a3cbcccbd013365bb
Brian Kim <briandhkim93@gmail.com>
2024-07-05 00:42:06 -0700

    Ep3 & 4 - url design and api versioning

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,13 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\ApiLoginRequest;
 use App\Traits\ApiResponses;
-use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use ApiResponses;
 
-    public function login(ApiLoginRequest $request) 
+    public function login(ApiLoginRequest $request)
     {
         return $this->ok($request->get('email'));
     }
--- app/Traits/ApiResponses.php
+++ app/Traits/ApiResponses.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 
 namespace App\Traits;
 
-
-trait ApiResponses {
-    protected function ok($message) {
+trait ApiResponses
+{
+    protected function ok($message)
+    {
         return $this->success($message, 200);
     }
 
-    protected function success($message, $statusCode = 200) {
+    protected function success($message, $statusCode = 200)
+    {
         return response()->json([
             'message' => $message,
-            'status' => $statusCode
+            'status' => $statusCode,
         ], $statusCode);
     }
 }
