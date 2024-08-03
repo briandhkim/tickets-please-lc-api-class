abae5e027824d683a567bb20d3be17022f89e9a2
Brian Kim <briandhkim93@gmail.com>
2024-08-03 00:25:15 -0700

    Ep18 - setup policies for user authorization

--- app/Http/Controllers/Api/V1/ApiController.php
+++ app/Http/Controllers/Api/V1/ApiController.php
@@ -4,10 +4,22 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Traits\ApiResponses;
+use Illuminate\Support\Facades\Gate;
 
 class ApiController extends Controller
 {
     use ApiResponses;
+
+    protected $policyClass;
+
+    protected string $namespace = 'App\\Policies\\V1';
+
+    public function __construct()
+    {
+        Gate::guessPolicyNamesUsing(
+            fn (string $modelClass) => "{$this->namespace}\\".class_basename($modelClass).'Policy'
+        );
+    }
 
     public function include(string $relationship): bool
     {
@@ -20,5 +32,10 @@ class ApiController extends Controller
         $includeValues = explode(',', strtolower($param));
 
         return in_array(strtolower($relationship), $includeValues);
+    }
+
+    public function isAble($ability, $targetModel)
+    {
+        return Gate::authorize($ability, $targetModel);
     }
 }
